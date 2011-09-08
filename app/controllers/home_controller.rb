0fec88eee21112ab3b4128dc6ce661411d3c3a6e
Michael Mainguy <michaelmainguy@Michael-Mainguys-MacBook-Pro.local>
2011-09-08 07:47:40 -0500

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  before_filter :authenticate_user!
+  def index
+    render :text=>"Welcome #{current_user.email}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeTracker::Application.config.secret_token = 'bdf364ee0910b32095513c7424858ddffaa12a9ae1a3648b59029355972b42cd2a203efee2d1aba1575c344f50f030968649469fd61d1e6d52afcb1f9cc746ea'
